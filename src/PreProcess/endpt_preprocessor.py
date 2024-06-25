158b423c247380e672de0595e400db0e25ea24b1
dineshiitbhu <147659243+dineshiitbhu@users.noreply.github.com>
2024-06-25 12:41:39 +0530

    Added patient overlap functionality for endpoint preprocessor

--- src/PreProcess/endpt_preprocessor.py
+++ src/PreProcess/endpt_preprocessor.py
@@ -5,15 +5,15 @@ from shutil import move
 import src.CustomLogger.custom_logger
 import src.Connectors.gdc_parser as gdc_prs
 
-class EndptPreProcessor(gdc_prs.GDCParser):
-    def __init__(self):
-        super().__init__()
+class EndptPreProcessor:
+    def __init__(self,data:pd.DataFrame=None):
+        self.data = data
         pass 
 
     def create_data_matrix(self):
         raise NotImplementedError("Method not implemented yet")
     
-    def get_patient_overlap(self):
+    def get_patient_overlap(self,data):
         """
         Args: Dataframe of patient data
 
@@ -21,6 +21,16 @@ class EndptPreProcessor(gdc_prs.GDCParser):
 
         pat_ovr = {"Sample type": pd.DataFrame, "Project Type": pd.DataFrame, "Sample type": pd.DataFrame}
         """
-        raise NotImplementedError("Method not implemented yet")
-    
+        categorical_variables = ['Sample Type','Project ID']
+        pat_over = {}
+        if data is None:
+            data=self.data
+        for category in categorical_variables:
+            if category in data.columns:
+                category_counts = data.groupby(category)['Case ID'].nunique().reset_index()
+                category_counts.columns = [category, 'Unique_Patient_Count']
+                pat_over[category] = category_counts
+        
+        return pat_over
+       
     
