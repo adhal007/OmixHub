4d88c9e04168238cdf187fe23e09b0533077c8e5
Abhilash Dhal <adhalbiophysics@gmail.com>
2024-08-29 22:24:00 +0530

    codeflash example on bubble-sort function

---
+++ tests/test_bubble_sort.py
@@ -0,0 +1,14 @@
+import unittest
+from src.bubble_sort import sorter
+
+class BubbleSort(unittest.TestCase):
+    def test_sort(self):
+        input = [5, 4, 3, 2, 1, 0]
+        output = sorter(input)
+        self.assertEqual(output, [0, 1, 2, 3, 4, 5])
+        input = [5.0, 4.0, 3.0, 2.0, 1.0, 0.0]
+        output = sorter(input)
+        self.assertEqual(output, [0.0, 1.0, 2.0, 3.0, 4.0, 5.0])
+        input = list(reversed(range(100)))
+        output = sorter(input)
+        self.assertEqual(output, list(range(100)))
