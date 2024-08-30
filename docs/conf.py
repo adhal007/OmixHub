775c5089983b45399a7d3d573c956c5d7bc3fac6
Abhilash Dhal <adhalbiophysics@gmail.com>
2024-08-30 13:47:35 +0530

    updated to include .github pattern for identification

--- docs/conf.py
+++ docs/conf.py
@@ -45,7 +45,7 @@ epub_show_urls = "footnote"
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", ".pytest_cache", ".github"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", ".pytest_cache"]
 
 # -- Options for HTML output -------------------------------------------------
 
